a48a3e396f19a55d932cab1b0b301714e4b4ce67
Arnogov <arnogov@gmail.com>
2020-04-16 12:11:47 +0200

    commit

--- tweet.py
+++ tweet.py
@@ -9,10 +9,10 @@ def createTweet():
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     authorName = db.Column(db.String(80), nullable=False)
-    content = db.Column(db.String(140), nullable=False )
-    image = db.Column(db.String(140), nullable=True )
+    content = db.Column(db.String(140), nullable=False)
+    image = db.Column(db.String(80), nullable=True)
     date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-   
+
     def print(self):
         print("-------------------------")
         print("@"+self.authorName + " said at "+str(self.date)+" :")
