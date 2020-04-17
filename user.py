6c855681ff23c414f05ed9b9e2af723b1a43baba
Arnogov <arnogov@gmail.com>
2020-04-17 10:43:03 +0200

    Change model

--- user.py
+++ user.py
@@ -3,7 +3,7 @@ from application import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(140), nullable=False)
+    email = db.Column(db.String(140), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=True)
-    tweets= db.relationship('Tweet', backref='user' ,lazy=True)
+    tweets = db.relationship('Tweet', backref='user', lazy=True)
     
